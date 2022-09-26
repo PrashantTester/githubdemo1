780b43b7f9b2f8ebdad2b5d5f6c2bbb2f83239c3
PrashantTester <Prashantt0071@gmail.com>
2022-09-26 16:56:51 +0530

    first commit demo

---
+++ Practice/src/Selenium/Demo2.java
@@ -0,0 +1,40 @@
+package Selenium;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Demo2 {
+
+	public static void main(String[] args) throws IOException {
+String path = "C:\\Users\\HP\\OneDrive\\Desktop\\Book1.xlsx";
+		
+		File file = new File (path);
+		
+		FileInputStream fis = new FileInputStream(file);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		
+		XSSFSheet sheet = wb.getSheetAt(0);
+		
+		String username = sheet.getRow(1).getCell(0).getStringCellValue();
+		System.out.println("usernameis "+username);
+		
+		String pass = sheet.getRow(1).getCell(1).getStringCellValue();
+		System.out.println("pass is "+pass);
+		
+		//close
+		wb.close();
+		
+		
+		
+
+	}
+
+	}
+
+
