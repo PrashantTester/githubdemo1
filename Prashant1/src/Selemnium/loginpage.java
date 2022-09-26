e3c05fbecb39b05bbbd7ba07376f973463e36bd3
PrashantTester <Prashantt0071@gmail.com>
2022-09-26 21:58:22 +0530

    Eclipse changes

--- Prashant1/src/Selemnium/loginpage.java
+++ Prashant1/src/Selemnium/loginpage.java
@@ -5,6 +5,8 @@ public class loginpage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("login page ");
+		System.out.println("new comment  page ");
+
 
 	}
 
